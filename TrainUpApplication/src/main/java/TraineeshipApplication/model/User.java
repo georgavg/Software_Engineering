9b48c4c5d1ce70debfe6196bafe77afcced2159e
OlgaMld <olgamld67@gmail.com>
2025-05-12 23:50:42 +0300

    Add model folder and User.java class

---
+++ TrainUpApplication/src/main/java/TraineeshipApplication/model/User.java
@@ -0,0 +1,71 @@
+package TraineeshipApplication.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "email_verified")
+    private boolean emailVerified;
+    
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    public User(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.emailVerified = false;
+    }
+
+    // --- Spring Security overrides ---
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired()     { return true; }
+    @Override
+    public boolean isAccountNonLocked()      { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    // Always allow login (don't block at login if unverified).
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
